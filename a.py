26af438765815af8ff35a684adf235ab20104caf
Summer94 <1192240895@qq.com>
2020-08-21 20:45:52 +0800

    44

---
+++ a.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/21 20:44
+# @Author  : summer
+# @File    : a.py
+# @Software: PyCharm
+a = 1
+def foo():
+    if a == 2:
+        print(2222)
+    else:
+        print(1111)
+
+foo()
